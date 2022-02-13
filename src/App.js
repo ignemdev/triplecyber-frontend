595c5d73029db52d24b0b4b47d3425125583eb55
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-12 23:03:49 -0400

    app bar added using material design
    -all elements margin removal
    -material icons installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import {
+  AppBar,
+  Toolbar,
+  Typography
+} from '@mui/material'
+import MovieIcon from '@mui/icons-material/Movie'
 
 function App() {
   return (
-    <div className='container'>
-      <h3 className=''></h3>
-    </div>
+    <AppBar position="relative">
+      <Toolbar variant="dense">
+        <MovieIcon sx={{ mr: 2 }} />
+        <Typography variant="h6" color="inherit" component="div">
+          Movies App
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 

ce8ff13d04053e2359cc0866e2839366c2d95b56
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-13 03:45:56 -0400

    pagination implemented

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,6 +1,6 @@
-import { Grid } from '@mui/material'
+import { Grid, Pagination, Container } from '@mui/material'
 import MovieItem from './MovieItem'
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import env from "react-dotenv";
 
 function Movies() {
@@ -8,25 +8,42 @@ function Movies() {
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState();
 
-    useEffect(() => {
+    const fetchData = () => {
         fetch(`${env.BASE_MOVIES_API}?page=${page}`)
             .then(res => res.json())
             .then(json => {
                 setMovies(json.results)
                 setTotalPages(json.totalPages)
             })
+    }
+
+    useEffect(() => {
+        fetchData();
     }, [])
 
+    const handlePageChange = (e, page) => {
+        setPage(page)
+        fetchData()
+    };
+
     return (
-        <Grid container spacing={2} padding={5}>
-            {movies.map(m => <MovieItem
-                key={m.id}
-                title={m.title}
-                voteAverage={m.voteAverage}
-                shortTitle={m.shortTitle}
-                backdropPath={m.backdropPath} />
-            )}
-        </Grid >
+        <Fragment>
+            <Container sx={{ pt: 3, display: 'flex', justifyContent: 'center' }}>
+                <Pagination count={totalPages} page={page} onChange={handlePageChange} />
+            </Container>
+            <Grid container spacing={2} padding={5}>
+                {movies.map(m => <MovieItem
+                    key={m.id}
+                    title={m.title}
+                    voteAverage={m.voteAverage}
+                    shortTitle={m.shortTitle}
+                    backdropPath={m.backdropPath} />
+                )}
+            </Grid >
+            <Container sx={{ pt: 3, display: 'flex', justifyContent: 'center' }}>
+                <Pagination count={totalPages} page={page} onChange={handlePageChange} />
+            </Container>
+        </Fragment>
     )
 }
 
